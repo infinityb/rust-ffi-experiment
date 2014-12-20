c4f0e6d612abc3237caee6e0b70094d5ba50d600
Stacey Ell <stacey.ell@gmail.com>
2014-12-19 20:19:48 -0600

    tabs -> spaces

--- src/lib.rs
+++ src/lib.rs
@@ -15,29 +15,29 @@ pub extern "C" fn doubleit(x: c_uint) -> c_uint {
 
 #[no_mangle]
 pub extern "C" fn count_ones(vals: *mut c_uchar, len: size_t) -> c_int {
-	if vals.is_null() {
-		return 0;
-	}
+    if vals.is_null() {
+        return 0;
+    }
 
-	let mut svals: &mut [u8] = unsafe {
-		mem::transmute(std::raw::Slice {
-			data: vals,
-			len: len as uint, // ???
-		})
-	};
+    let mut svals: &mut [u8] = unsafe {
+        mem::transmute(std::raw::Slice {
+            data: vals,
+            len: len as uint, // ???
+        })
+    };
 
-	let mut alloced = Vec::with_capacity(len as uint);
+    let mut alloced = Vec::with_capacity(len as uint);
 
-	let mut counter = 0;
-	for (idx, val) in svals.iter_mut().enumerate() {
-		if *val == 1 {
-			alloced.push(idx);
-			counter += 1;
-			// replaces the 1s with their index mod 256, cuz mut.
-			*val = (idx % 256) as u8;
-		}
-		
-	}
-	println!("alloced = {}", alloced);
+    let mut counter = 0;
+    for (idx, val) in svals.iter_mut().enumerate() {
+        if *val == 1 {
+            alloced.push(idx);
+            counter += 1;
+            // replaces the 1s with their index mod 256, cuz mut.
+            *val = (idx % 256) as u8;
+        }
+        
+    }
+    println!("alloced = {}", alloced);
     counter
 }
