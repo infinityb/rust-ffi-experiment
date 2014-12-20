c038a03f0217a230a958df6d0043d2fa9ec8723a
Stacey Ell <stacey.ell@gmail.com>
2014-12-19 20:09:37 -0600

    Use libc types

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,20 @@
 extern crate libc;
-use libc::c_int;
+
+use libc::{c_int, c_uchar, c_uint, size_t};
 use std::mem;
 
 #[no_mangle]
-pub extern "C" fn foo() -> uint {
+pub extern "C" fn foo() -> c_uint {
     4
 }
 
 #[no_mangle]
-pub extern "C" fn doubleit(x: uint) -> uint {
+pub extern "C" fn doubleit(x: c_uint) -> c_uint {
     2 * x
 }
 
 #[no_mangle]
-pub extern "C" fn count_ones(vals: *mut u8, len: uint) -> c_int {
+pub extern "C" fn count_ones(vals: *mut c_uchar, len: size_t) -> c_int {
 	if vals.is_null() {
 		return 0;
 	}
@@ -21,18 +22,18 @@ pub extern "C" fn count_ones(vals: *mut u8, len: uint) -> c_int {
 	let mut svals: &mut [u8] = unsafe {
 		mem::transmute(std::raw::Slice {
 			data: vals,
-			len: len
+			len: len as uint, // ???
 		})
 	};
 
-	let mut alloced = Vec::with_capacity(len);
+	let mut alloced = Vec::with_capacity(len as uint);
 
 	let mut counter = 0;
 	for (idx, val) in svals.iter_mut().enumerate() {
 		if *val == 1 {
 			alloced.push(idx);
 			counter += 1;
-			// replaces the 1s with their index mod 256
+			// replaces the 1s with their index mod 256, cuz mut.
 			*val = (idx % 256) as u8;
 		}
 		
